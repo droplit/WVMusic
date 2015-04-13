f8dd92de8dd9078bda1aa4c047ca099273ab6490
jpinz <julian.pinzer@gmail.com>
2015-04-12 21:04:57 -0400

    Initial Commit.

---
+++ app/src/main/java/com/droplit/wave/MainActivity.java
@@ -0,0 +1,155 @@
+package com.droplit.wave;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
+import com.droplit.wave.adapters.RecyclerViewAdapter;
+import com.melnykov.fab.FloatingActionButton;
+import com.melnykov.fab.ObservableScrollView;
+import com.melnykov.fab.ScrollDirectionListener;
+import io.fabric.sdk.android.Fabric;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        initActionBar();
+    }
+
+    private void initActionBar() {
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setBackgroundDrawable(new ColorDrawable(R.color.primary));
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.addTab(actionBar.newTab()
+                    .setText("RecyclerView")
+                    .setTabListener(new ActionBar.TabListener() {
+                        @Override
+                        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                            fragmentTransaction.replace(android.R.id.content, new SongViewFragment());
+                        }
+
+                        @Override
+                        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                        }
+
+                        @Override
+                        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                        }
+                    }));
+            actionBar.addTab(actionBar.newTab()
+                    .setText("ScrollView")
+                    .setTabListener(new ActionBar.TabListener() {
+                        @Override
+                        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                            fragmentTransaction.replace(android.R.id.content, new ScrollViewFragment());
+                        }
+
+                        @Override
+                        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                        }
+
+                        @Override
+                        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                        }
+                    }));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class SongViewFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.fragment_song, container, false);
+
+            RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recycler_view);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+
+            RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity(), getResources()
+                    .getStringArray(R.array.songs));
+            recyclerView.setAdapter(adapter);
+
+            FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
+            fab.attachToRecyclerView(recyclerView);
+
+            return root;
+        }
+    }
+
+    public static class ScrollViewFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.fragment_scrollview, container, false);
+
+            ObservableScrollView scrollView = (ObservableScrollView) root.findViewById(R.id.scroll_view);
+            LinearLayout list = (LinearLayout) root.findViewById(R.id.list);
+
+            String[] songs = getResources().getStringArray(R.array.songs);
+            for (String song : songs) {
+                TextView textView = (TextView) inflater.inflate(R.layout.song_item, container, false);
+                String[] values = song.split(",");
+                String songName = values[0];
+                int flagResId = getResources().getIdentifier(values[0], "drawable", getActivity().getPackageName());
+                textView.setText(songName);
+                textView.setCompoundDrawablesWithIntrinsicBounds(flagResId, 0, 0, 0);
+
+                list.addView(textView);
+            }
+
+            FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
+            fab.attachToScrollView(scrollView);
+
+            return root;
+        }
+    }
+}
